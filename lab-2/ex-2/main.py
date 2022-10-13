10a00b9d82e1904a4c26358362231d245ef28e36
Ciszkoo <pawel.olszko.p@gmail.com>
2022-10-13 17:03:00 +0200

    exercise 2 lab 2 fix

--- lab-2/ex-2/main.py
+++ lab-2/ex-2/main.py
@@ -12,11 +12,11 @@ sshClient.connect(hostname=host, port=port, username=username)
 path = input("Enter path: ")
 
 stdin, stdout, stderr = sshClient.exec_command("cat " + path)
-lines = stdout.readlines()
-err = stderr.readlines()
+exitCode = stdout.channel.recv_exit_status()
 
-if len(err) != 0:
-    print("File not found")
-else:
+if exitCode == 0:
+    lines = stdout.readlines()
     for line in lines:
         print(line, end="")
+else:
+    print("Error: " + str(exitCode))
